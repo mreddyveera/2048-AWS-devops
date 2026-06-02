c578dd42a81a2d11d3b916983ad3d10cc7e37bb8
mreddyveera <reddyveeramanikanta54304@gmail.com>
2026-06-02 11:33:01 +0530

    feature/RV-10001:Updated getItem function in local_storage.js

--- src/js/local_storage_manager.js
+++ src/js/local_storage_manager.js
@@ -6,7 +6,7 @@ window.fakeStorage = {
   },
 
   getItem: function (id) {
-    return this._data.hasOwnProperty(id) ? this._data[id] : undefined;
+    return Object.prototype.hasOwnProperty.call(this._data,id) ? this._data[id] : undefined;
   },
 
   removeItem: function (id) {
