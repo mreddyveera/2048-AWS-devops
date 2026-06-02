053610c8f6ecc64d65e30c87918e02aae0dfc88e
mreddyveera <reddyveeramanikanta54304@gmail.com>
2026-06-02 11:27:36 +0530

    feature/RV10001:Updated eslint.config.json

--- src/eslint.config.mjs
+++ src/eslint.config.mjs
@@ -1,7 +1,28 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+// import js from "@eslint/js";
+// import globals from "globals";
+// import { defineConfig } from "eslint/config";
+
+// export default defineConfig([
+//   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+// ]);
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        GameManager: 'readonly',
+        KeyboardInputManager: 'readonly',
+        HTMLActuator: 'readonly',
+        LocalStorageManager: 'readonly',
+        Grid: 'readonly',
+        Tile: 'readonly'
+      }
+    }
+  }
 ]);
