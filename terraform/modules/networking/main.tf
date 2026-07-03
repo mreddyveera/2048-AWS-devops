f04184447f1445c12f88325195877a6bb403287a
Reddy  Veera Manikanta <reddyveeramanikanta54304@gmail.com>
2026-07-04 01:25:04 +0530

    Update flow log resource to use IAM role and log group

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -15,8 +15,8 @@ resource "aws_vpc" "vpc" {
   )
 }
 resource "aws_flow_log" "example" {
-  iam_role_arn    = "arn"
-  log_destination = "log"
+  iam_role_arn    = aws_iam_role.vpc_flow_log_role.arn
+  log_destination = aws_cloudwatch_log_group.vpc_flow_logs.arn
   traffic_type    = "ALL"
   vpc_id          = aws_vpc.vpc.id
 }
