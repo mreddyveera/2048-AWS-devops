2106aeb67c55029c3a2b1f6b965d240d56d07854
mreddyveera <reddyveeramanikanta54304@gmail.com>
2026-07-04 00:48:46 +0530

    Terraform code formatted

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "2048-game-terraform-locks" 
-    key    = "dev/terraform.tfstate"
-    region = "us-east-1"
-    encrypt        = true
+    bucket  = "2048-game-terraform-locks"
+    key     = "dev/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
     #dynamodb_table = "2048-game-terraform-locks"
     use_lockfile = true
   }
